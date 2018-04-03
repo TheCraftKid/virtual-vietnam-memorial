e8e6a485e1ced0d969658639a83668340cb2dae8
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-02 22:50:50 -0500

    Reformat App.vue  - Set up app structure

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <md-app>
+    <md-app-toolbar>
+      <router-link class="md-title" to="/">Virtual Vietnam Memorial</router-link>
+    </md-app-toolbar>
+    <md-app-content>
+      <router-view />
+    </md-app-content>
+  </md-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+<style lang="scss">
+
+.md-app {
+  height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.md-app-content {
+  height: 100%;
 }
 </style>
