4c5cc67a766d9a30d72b06a2bab7d27c7c6a5180
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-02 22:49:48 -0500

    Use vue-material

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+Vue.use(VueMaterial);
 Vue.config.productionTip = false;
 
 new Vue({
