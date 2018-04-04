fdfdbde1b14a9a446c0bf310efb7b20d5671a52f
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-03 21:33:37 -0500

    Initialize Firebase in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import * as firebase from 'firebase';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 import App from './App.vue';
@@ -9,6 +10,15 @@ import './registerServiceWorker';
 
 Vue.use(VueMaterial);
 Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC3KJEOaHk4qoRYBMrPt6wtUv4uq6ttV50',
+  authDomain: 'willie-personal-projects.firebaseapp.com',
+  databaseURL: 'https://willie-personal-projects.firebaseio.com',
+  projectId: 'willie-personal-projects',
+  storageBucket: 'virtual-vietnam-memorial',
+  messagingSenderId: '1050896936846',
+});
 
 new Vue({
   router,
