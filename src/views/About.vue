22944b1a36bc4dfb7a8320ea31dcabb71686d000
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-04 02:20:42 -0500

    Update About page  - Wrap content in material card  - Update styles  - Update some parts of writing style and add self attribution

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,49 +1,66 @@
 <template>
   <main>
-    <h2>About the Virtual Vietnam Memorial</h2>
-    <p>
-      The Virtual Vietnam Memorial is a project to dedicate the lives of those
-      fallen in the Vietnam War.
-    </p>
-    <p>
-      After reading <i>The Things They Carried</i>, my AP English Language
-      class began a project to honor the fallen soldiers in the Vietnam War.
+    <md-card>
+      <h2>About This Project</h2>
+      <p>
+        The Virtual Vietnam Memorial is a project by Willie Chalmers III to
+        dedicate the lives of those fallen in the Vietnam War.
+      </p>
+      <p>
+        After reading <i>The Things They Carried</i> by Tim O'Brien, a man who
+        served in the Vietnam War, my AP English Language class began a project
+        to honor the fallen soldiers in the Vietnam War: we'd all make videos
+        about a solider we chose, whether because he was an interesting man
+        all-around or he had some personal connection to one of us.
 
-      I immediately thought about how I'd present my soldier, but I remembered
-      an unfortunate part of the presentation process.
-    </p>
-    <p>
-      The time it takes between presentations leaves a lot of room to forget.
-      How come is it, in 2018, that sharing multimedia presentations is
-      difficult not only for the teacher, but for the student?
+        I immediately thought about how I'd present my soldier, but I remembered
+        an unfortunate part of the presentation process.
+      </p>
+      <p>
+        The time it takes between presentations leaves a lot of room to forget.
+        How come is it, in 2018, that sharing multimedia presentations is
+        difficult not only for the teacher, but for the student?
 
-      Yes, there's <a href="https://www.youtube.com" target="_blank">YouTube</a>,
-      but not everyone wishes to upload their school projects to YouTube. Many
-      students and teachers would rather have students bring in a flash drive
-      anyway.
-    </p>
-    <p>
-      And what about after the presentations? Are they lost forever? Why can't
-      they be preserved for those well beyond our presentation days.
-    </p>
-    <p>
-      Then came this idea a few minutes later: the idea of a virtual memorial.
-      In this virtual memorial, students could upload their videos to a trusted
-      site anyone could use for more than just a single presentation.
-    </p>
-    <p>I went ahead and built this in a day because, as Tim O'Brien said:</p>
-    <blockquote>
-      In a story, miracles can happen.
-    </blockquote>
-    <p>
-      And in this case, I intend to preserve the stories of the fallen in a
-      manner in which I know how.
-    </p>
+        Yes, there's <a href="https://www.youtube.com" target="_blank">YouTube</a>,
+        but not everyone wishes to upload their school projects to YouTube. Many
+        students and teachers would rather have students bring in a flash drive
+        anyway.
+      </p>
+      <p>
+        And what about after the presentations? Are they lost forever? Why
+        can't they be preserved for those interested souls well beyond our
+        presentation days? Why does remembering have to stop after we get a
+        grade for a project?
+      </p>
+      <p>
+        Then came this idea a few minutes later: the idea of a virtual memorial.
+        In this virtual memorial, students could upload their videos to a trusted
+        site anyone could use for more than just a single presentation.
+      </p>
+      <p>I went ahead and built this in a day because, as Tim O'Brien said:</p>
+      <blockquote>
+        <em>In a story, miracles can happen.</em>
+      </blockquote>
+      <p>
+        And in this case, I intend to preserve the stories of the fallen in a
+        manner in which I know how.
+      </p>
+    </md-card>
   </main>
 </template>
 
-<style>
+<style scoped>
+main {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
 blockquote {
   /* TODO: Style blockquote differently */
+}
+
+.md-card {
+  padding: 24px 16px 16px;
+  max-width: 600px;
 }
 </style>
