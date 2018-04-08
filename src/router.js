8b2d6fa5540b98f273c07651ef393a92bdfe9da4
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-08 11:12:45 -0500

    Update router  - Add routes for view components

--- src/router.js
+++ src/router.js
@@ -1,21 +1,45 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import PresentationHome from './views/PresentationHome.vue';
+import PresentationView from './views/PresentationView.vue';
+import UploadScreen from './views/UploadScreen.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: PresentationHome,
+    },
+    {
+      path: '/period/:classPeriod',
+      name: 'home-period',
+      props: true,
+      component: PresentationHome,
     },
     {
       path: '/about',
       name: 'about',
       component: About,
+    },
+    {
+      path: '/upload',
+      name: 'upload',
+      component: UploadScreen,
+    },
+    {
+      path: '/present/:id',
+      name: 'present',
+      props: true,
+      component: PresentationView,
+    },
+    {
+      path: '/present',
+      alias: 'present',
     },
   ],
 });
