47e4b9faf3f1d37c3ae19a055d5b9f47bfdca33d
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-08 23:00:56 -0500

    Create function to handle thumbnail fetching

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,25 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { CallableContext, HttpsError } from 'firebase-functions/lib/providers/https';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const THUMBNAIL_LIMIT = 10;
+
+export const getThumbnails = functions.https.onCall((data, context: CallableContext) => {
+  const classObject = data['class'];
+  let query: FirebaseFirestore.Query;
+  if (classObject) {
+    query = admin.firestore().collection('presentations')
+      .where('period', '==', classObject.period);
+  } else {
+    query = admin.firestore().collection('presentations');
+  }
+  return query.limit(THUMBNAIL_LIMIT).get()
+    .then((snapshots) => snapshots.docs.map((snap) => {
+      const presentation = snap.data();
+      return presentation.thumbnailUrl;
+    })).catch((err) => {
+      console.error('Error when fetching thumbnails', err);
+      return new HttpsError('internal',
+        'The function encountered a server error when fetching thumbnails');
+    });
+});
