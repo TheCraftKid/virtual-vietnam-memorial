b546d9de9b61c3960e59e1cc58a6934ad34c92bd
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-08 22:08:44 -0500

    Add documentation to utils

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,6 +3,8 @@ import * as firebase from 'firebase';
 /**
  * Ensures that the `firebase` object has an app associated.
  * Should never throw error in normal circumstances.
+ *
+ * @throws {Error} When `firebase` hasn't been initialized
  */
 export function checkForFirebase() {
   if (firebase.app == null) {
@@ -10,6 +12,11 @@ export function checkForFirebase() {
   }
 }
 
+/**
+ * Creates a new unique idenitier used for video IDs.
+ *
+ * @return {String} A pseduo-random globally unique identifier
+ */
 export function generateUUID() {
   return Math.floor((Math.random() + 1) * 0x10000)
     .toString(16)
