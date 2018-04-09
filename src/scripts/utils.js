6def1498f05a21f3ff22a8ffd23ada76dce92cff
Willie Chalmers III <willieechalmers@gmail.com>
2018-04-08 22:06:39 -0500

    Use explicit exports in utils

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -4,19 +4,14 @@ import * as firebase from 'firebase';
  * Ensures that the `firebase` object has an app associated.
  * Should never throw error in normal circumstances.
  */
-function checkForFirebase() {
+export function checkForFirebase() {
   if (firebase.app == null) {
     throw new Error('FirebaseApp not initialized');
   }
 }
 
-function generateUUID() {
+export function generateUUID() {
   return Math.floor((Math.random() + 1) * 0x10000)
     .toString(16)
     .substring(1);
 }
-
-export default {
-  checkForFirebase,
-  generateUUID,
-};
